31823ffffbab35a685543b6ccb5ff6306e22f85d
羊吃饭 <554518842@qq.com>
2021-04-12 10:00:36 +0800

    init

---
+++ src/main/java/com/epichust/daq/DaqApplication.java
@@ -0,0 +1,13 @@
+package com.epichust.daq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epichust/daq/DaqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epichust.daq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
